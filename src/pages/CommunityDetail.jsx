73b8995f38b44075c0555b28771f8dd6b6206abb
gueon7866 <lsg7866@gmail.com>
2025-08-13 15:07:52 +0900

    Update CommunityDetail.jsx

--- src/pages/CommunityDetail.jsx
+++ src/pages/CommunityDetail.jsx
@@ -6,7 +6,7 @@ const CommunityDetail = () => {
   const nav = useNavigate()
   return (
     <section>
-        <h1>게시글 상세</h1>
+        <h1>커뮤니티 상세</h1>
         <p>현재 글 ID :{id} </p>
         <button onClick={()=>nav(-1)}>뒤로 가기</button>
         <button onClick={()=>nav('/')}>홈으로 가기</button>
